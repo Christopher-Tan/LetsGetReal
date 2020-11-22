31a6ecfe78d89c2c5be246c9160403d48e26ef15
Christopher Tan <ctan20@stuy.edu>
2020-11-22 07:22:03 -0500

    Added and tested equals method for realnumbers

--- Tester.java
+++ Tester.java
@@ -1,5 +1,13 @@
 public class Tester {
     public static void main(String[] args) {
-        
+        //EQUALS
+        RealNumber rn1 = new RealNumber(0.0001);
+        RealNumber rn2 = new RealNumber(0.01);
+        System.out.println(rn1.equals(rn2));
+        RealNumber rn3 = new RealNumber(1);
+        RealNumber rn4 = new RealNumber(1.00001);
+        RealNumber rn5 = new RealNumber(1.000009);
+        System.out.println(rn3.equals(rn4));
+        System.out.println(rn3.equals(rn5));
     }
 }
